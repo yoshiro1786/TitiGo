890177b658173e0e3f0e74f605ce1041c36bdc19
Yoshiro Milton Miranda Valdivia <yoshiromiltonmirandavaldivia@Mac-mini-de-Yoshiro.local>
2025-09-11 10:22:11 -0500

    Primera subida

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,10 +1,10 @@
 // An array of links for navigation bar
 const navBarLinks = [
-  { name: "Home", url: "/" },
-  { name: "Products", url: "/products" },
-  { name: "Services", url: "/services" },
-  { name: "Blog", url: "/blog" },
-  { name: "Contact", url: "/contact" },
+  { name: "Inicio", url: "/" },
+  // { name: "Products", url: "/products" },
+  // { name: "Services", url: "/services" },
+  // { name: "Blog", url: "/blog" },
+  // { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
@@ -28,7 +28,7 @@ const footerLinks = [
 ];
 // An object of links for social icons
 const socialLinks = {
-  facebook: "https://www.facebook.com/",
+  facebook: "https://www.facebook.com/share/16r1MHuweW/?mibextid=wwXIfr",
   x: "https://twitter.com/",
   github: "https://github.com/mearashadowfax/ScrewFast",
   google: "https://www.google.com/",
