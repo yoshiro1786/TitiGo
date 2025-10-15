1382559e8617c48af313aca131f81d48c32b25bf
yoshiro1786 <65630213+yoshiro1786@users.noreply.github.com>
2025-10-14 23:25:36 -0500

    Update constants.ts

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -2,11 +2,11 @@ import ogImageSrc from "@images/social.png";
 
 export const SITE = {
   title: "TitiGo",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  tagline: "TitiGO | Transporte privado en Arequipa - Traslados aeropuerto, hoteles y más",
+  description: "En TitiGo ofrecemos transporte privado en Arequipa, con recojo en el aeropuerto, hoteles, eventos y más. Reserva tu movilidad ejecutiva hoy mismo.",
+  description_short: "TitiGo Ofrecemos transporte privado en Arequipa, con recojo en aeropuerto Rodríguez Ballón, hoteles, eventos y más.",
+  url: "https://titigo.netlify.app",
+  author: "Yoshiro Miranda",
 };
 
 export const SEO = {
